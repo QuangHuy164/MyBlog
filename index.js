a2931bafdc7804fe8467e796872d42310c67f431
QuanqHuy <90618309+QuangHuy164@users.noreply.github.com>
2025-05-16 15:37:56 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-console.log('Hello');
-console.log('I like kebab');
-
-window.alert('alert');
-window.alert('I like kebab');
-
-document.getElementById('myH1').textContent = 'Hello';
-document.getElementById('myP').textContent = 'I like kebab';
